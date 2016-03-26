44dd6e0ecdda4144d18e09f7d7dda37720f69ff8
Zac Steer <steerzac@gmail.com>
2016-03-26 12:14:51 -0500

    Configured with bootstrap

--- contact.php
+++ contact.php
@@ -1,12 +1,55 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Apply</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="shortcut icon" href="Images/smiley.png">
+    <script src="jss/script.js"></script>
+</head>
+
+<body>
+   <div class="container">
+    <div id="wrapper">
+
+        <div class="page-header">
+            <!--<img style="float:right" src="acc.jpeg" alt="description" title="acc-logo" width="70" height="70" />-->
+            <nav class="navbar navbar-default navbar-fixed-top" style="background-color:#500000;">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a style="color:#ffffff;" class="navbar-brand" href="index.html">Aggie Coding Club</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse navbar-right">
+          <ul class="nav navbar-nav">
+            <li class="nav-item"><a style="color:#ffffff;" href="index.html">Home</a></li>
+            <li class = "nav-item"><a style="color:#ffffff;" href="about.html">About</a></li>
+            <li class = "nav-item"><a style="color:#ffffff;" href="resources.html">Resources</a></li>
+            <li class = "nav-item"><a style="color:#ffffff;" href="projects.html">Projects</a></li>
+            <li class = "active nav-item"><a style="color:#ffffff; background-color:#b3b3b3" href="#">Apply</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+        </div>
+
+       <?php
    if(isset($_POST["submit"])) {
     $FirstName = $_POST["FirstName"];
     $LastName = $_POST["LastName"];
     $Email = $_POST["Email"];
+    $Phone = $_POST["Phone"];
        
     $FirstNameText = "First Name: " . $FirstName . "\n";
     $LastNameText = "Last Name: " . $LastName . "\n";
     $EmailText = "Email: " . $Email . "\n";
+    $PhoneText = "Phone: " . $Phone . "\n";
        
     if(isset($_POST["Classification"])) {
         $Classification = $_POST["Classification"];
@@ -15,80 +58,61 @@
     if(isset($_POST["MessageOfText"])) {
         $MessageOfText = "Message: " . $_POST["MessageOfText"];
     }
-    $sentMail = mail("steerzac@tamu.edu, aggiecodingclub@gmail.com", "ACC Website Interest Form", $FirstNameText. $LastNameText . $EmailText . $ClassificationText . $MessageOfText);
-    if($sentMail) {
-        echo "Contact form received. Thank you, " . $FirstName;
-    } else {
-        die("Form did not send");
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Apply</title>
-    <link rel="stylesheet" href="css/contact.css">
-    <link rel="shortcut icon" href="Images/smiley.png">
-    <script src="jss/script.js"></script>
-</head>
-
-<body>
-    <div id="wrapper">
-
-        <div id="header">
-            <!--<img style="float:right" src="acc.jpeg" alt="description" title="acc-logo" width="70" height="70" />-->
-            <div id="title_logo">
-                <h1>Aggie Coding Club</h1>
-            </div>
-            <section class="nav">
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="projects.html" title="Projects">Projects</a></li>
-                    <li><a href="resources.html" title="Resources">Resources</a></li>
-                    <li><a href="about.html" title="Info">About Us</a></li>
-                    <li><a href="contact.php" title="Apply">Apply</a>
-                    </li>
-                </ul>
-            </section>
-        </div>
-
        
+    if (mail ("steerzac@tamu.edu", "ACC Website Interest Form", $FirstNameText. $LastNameText . $EmailText . $ClassificationText . $PhoneText . $MessageOfText)) {
+        echo '<div class="alert alert-success">Thank You! An officer will contact you shortly</div>';
+    } else {
+        echo '<div class="alert alert-danger">Sorry there was an error sending your message. We are working to fix the problem</div>';
+    }
+}      
+       
+       
+
+?>
 
         <div class="main">
             <!--            <img src="http://placehold.it/100x100">-->
 
-            <div class="message-form">
+            <div class="jumbotron">
                 <h2>Apply</h2>
                 <p id="Intro-para">Fill out the interest form below. An officer will contact you with more information about joining the club shortly after.</p>
 
                 <h3 id="Interest-Form-Header">Interest Form</h3>
-                <form id="userForm" action="contact.php" method="post">
+                <form class="form-horizontal" role="form" action="contact.php" method="post">
 
-                    <p>
-                        <label for="FirstName">First: </label>
-                        <input name="FirstName" type="text" placeholder="John" required>
-                    </p>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="FirstName">First: </label>
+                        <div class="col-sm-10">
+                        <input class="form-control" name="FirstName" type="text" placeholder="John" required>
+                        </div>
+                    </div>
                     
-                    <p>
-                        <label for="LastName">Last: </label>
-                        <input name="LastName" type="text" placeholder="Doe" required>
-                    </p>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="LastName">Last: </label>
+                        <div class="col-sm-10">
+                        <input class="form-control" name="LastName" type="text" placeholder="Doe" required>
+                        </div>
+                    </div>
 
-                    <p>
-                        <label for="Email">Email: </label>
-                        <input name="Email" type="email" placeholder="ACC@gigem.com" required>
-                    </p>
+                    <div class="form-group">
+                       
+                        <label class="control-label col-sm-2" for="Email">Email: </label>
+                        <div class="col-sm-10">
+                        <input name="Email" class="form-control" id="email" type="email" placeholder="ACC@gigem.com" required>
+                        </div>
+                    </div>
 
-                    <p>
-                    <label for="Phone">Phone: </label>
-                    <input name="Phone" type="tel" placeholder="999-999-9999">
-                    </p>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="Phone">Phone: </label>
+                        <div class="col-sm-10">
+                        <input class="form-control" name="Phone" type="tel" placeholder="999-999-9999">
+                        </div>
+                    </div>
 
-                    <p>
-                        <label for="Classification">Classification: </label>
-                        <select name="Classification">
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="Classification">Class: </label>
+                        <div class="col-sm-10">
+                          <select class="form-control" name="Classification">
                            <option value=""></option>
                             <option value="Freshman">Freshman</option>
                             <option value="Sophomore">Sophomore</option>
@@ -96,34 +120,39 @@
                             <option value="Senior">Senior</option>
                             <option value="Grad Student">Grad Student</option>
                         </select>
-                    </p>
+                        </div>
+                    </div>
 
 
-                    <p>
-                        <label for="MessageOfText">Message</label>
-                        <textarea name="MessageOfText" id="MessageOfText" form="userForm" cols="30" rows="5" placeholder="Why do you want to join the club? If you have any specific interests in coding, please list them"></textarea>
-                    </p>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="MessageOfText">Message</label>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" name="MessageOfText" id="MessageOfText" cols="30" rows="5" placeholder="Why do you want to join the club? If you have any specific interests in coding, please list them"></textarea>
+                        </div>
+                    </div>
 
-                    <p>
-                        <input name="submit" id="submit" type="submit">
-                    </p>
-
-
+                    <div class="form-group">
+                        <input type="submit" name="submit" class="form-control btn btn-default">
+                    </div>
                 </form>
-
             </div>
-
-
-            <p>
-             You can also email us directly at: <a href="mailto:aggiecodingclub@gmail.com">aggiecodingclub@gmail.com</a></p>
-
         </div>
 
-        <footer class="footer">
-            &copy; Aggie Coding Club | Page Designed By:
-            <a href="https://www.linkedin.com/in/zachary-steer-8b4196103" target="_blank">Zac Steer</a>
+        <footer class="well" style="text-align:center;">
+            <div>
+                <div>
+                 You can also email us directly at: <a href="mailto:aggiecodingclub@gmail.com" style="text-decoration:none; color:black;">aggiecodingclub@gmail.com</a>
+                </div>
+                &copy; Aggie Coding Club | Page Designed By:
+                <a href="https://steerzac.github.io" style="text-decoration:none; color:black;" target="_blank">Zac Steer</a>
+            </div>
         </footer>
     </div>
+    
+    </div>
+    <script src="js/jquery-2.2.1.js"></script>
+ 	<script src="js/bootstrap.min.js"></script>
+ 	<script src="js/hamburger.js"></script>
 </body>
 
 </html>
