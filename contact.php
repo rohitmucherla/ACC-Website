dce2aadd7549551cd911df4273fde6a9b6fb2247
steerzac <steerzac@gmail.com>
2016-08-02 20:38:17 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Apply</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="shortcut icon" href="Images/smiley.png">
-    <script src="jss/script.js"></script>
 </head>
 
 <body>
